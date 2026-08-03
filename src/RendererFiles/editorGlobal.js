1abe7dda26640d7befde16b4ec9e41007d661890
Luthetus <luthetus@gmail.com>
2026-08-03 18:36:22 -0400

    Cause I quoted it one time.

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -10104,4 +10104,7 @@ You cannot comprehend my point of view I've tried many times.
 
 " you can't break those cuffs" I Can't stand seeing this video recommended to me I know exactly why it is
 
+cause you know something I never even clicked on the video.
+It just keeps showing up. You know whyt? Cause I quoted it one time.
+
 */
