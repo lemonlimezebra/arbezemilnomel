b92018bf5556494c11928aca8f62d526e99d2bb9
Luthetus <luthetus@gmail.com>
2026-08-05 09:56:38 -0400

    rAF tooltip

--- src/RendererFiles/tooltipGlobal.js
+++ src/RendererFiles/tooltipGlobal.js
@@ -1,6 +1,43 @@
 let TOOLTIP_exists = false;
 
-function TOOLTIP_show(textContent) {
+let TOOLTIP_pending_textContent = null;
+
+/**
+ * 0 => None
+ * 1 => Show
+ * 2 => Hide
+ */
+let TOOLTIP_pending_renderKind = 0;
+let TOOLTIP_isRenderPending = false;
+
+function TOOLTIP_render_request(renderKind) {
+    TOOLTIP_pending_renderKind = renderKind;
+    
+    if (!TOOLTIP_isRenderPending) {
+        TOOLTIP_isRenderPending = true;
+        requestAnimationFrame(TOOLTIP_renderDo);
+    }
+}
+
+function TOOLTIP_renderDo() {
+    let renderKind = TOOLTIP_pending_renderKind;
+    TOOLTIP_pending_renderKind = 0;
+
+    if (renderKind === 1) {
+        TOOLTIP_render_do_show();
+    }
+    else if (renderKind === 2) {
+        TOOLTIP_render_do_hide();
+    }
+    else {
+        throw new Error();
+    }
+    
+    TOOLTIP_isRenderPending = false; // Reset the paint lock
+};
+
+function TOOLTIP_render_do_show() {
+    TOOLTIP_render_request(1);
 
     let tooltipElement;
 
@@ -28,17 +65,27 @@ function TOOLTIP_show(textContent) {
     // This was quickest first way of writing things that came to my mind.
     // I don't like it cause you're appending the child, then setting textContent
     // but it is sufficient for the first version.
-    tooltipElement.textContent = textContent;
+    tooltipElement.textContent = TOOLTIP_pending_textContent;
+    TOOLTIP_pending_textContent = null;
 
     TOOLTIP_exists = true;
 }
 
-function TOOLTIP_hide() {
+function TOOLTIP_show(textContent) {
+    TOOLTIP_pending_textContent = textContent;
+    TOOLTIP_render_request(1);
+}
 
+function TOOLTIP_render_do_hide() {
     const tooltip = document.getElementById('TOOLTIP');
     if (tooltip) {
         tooltip.remove();
     }
 
     TOOLTIP_exists = false;
+}
+
+function TOOLTIP_hide() {
+    TOOLTIP_pending_textContent = null;
+    TOOLTIP_render_request(2);
 }
