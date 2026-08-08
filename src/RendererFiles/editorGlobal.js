eb7b2f46c63a49a90e37dc53d710bce79c0850a0
Luthetus <luthetus@gmail.com>
2026-08-08 11:43:26 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -10450,4 +10450,6 @@ It is a matter of my parents having 0 understanding of me.
 And I really do mean that, not even in just some cringe way "nobody understands me"
 like I literally don't want your life nothing to do with it and nothing will fix that lack of desire
 
+I want to be a hermit. I want 0 contact with anyone.
+
 */
