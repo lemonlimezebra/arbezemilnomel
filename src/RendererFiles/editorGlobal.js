4df835a7fdc18c548e80822d457eb526869dd958
Luthetus <luthetus@gmail.com>
2026-08-08 10:15:58 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -10426,4 +10426,22 @@ I'm gonna eat 2 lbs of chicken today I'm gonna do it I'm so sick of everything.
 
 I'm gonna drive to the store and buy some more chicken right now.
 
+TODO: When you expand a node if the longest text exists equally it won't redraw with more padding and be wrong
+
+Breakfast:
+1 lb of 98% fat free chicken with sriracha
+
+Lunch:
+8x strawberry nutrigrain bars
+
+Dinner:
+1 lb of 98% fat free chicken with sriracha
+
+Do not eat anything else until you hit 199.9 lbs.
+
+2 lbs of 98% fat free chicken is $14
+The nutrigrain bar pack is $3
+
+total is $17
+
 */
